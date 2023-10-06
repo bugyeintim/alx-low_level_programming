36b96f88a7faee4dafc5a6c68d5c8ce4f4986c54
Kofi Ntim <bugyeintim@outlook.com>
2023-10-06 12:36:40 -0700

    a c program that prints a message to the standard output

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+
+/**
+ * main - this is the main entry point
+ *
+ * Return: 0
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
