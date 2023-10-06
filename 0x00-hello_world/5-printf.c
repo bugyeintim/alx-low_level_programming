1a4692e4b1df5bd4f212e0fd0c31f5057dc885ce
Kofi Ntim <bugyeintim@outlook.com>
2023-10-06 12:55:44 -0700

    a program that prints a sentence to the standard output

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+
+ /**
+  * main - this is the main printf function
+  *
+  * Return: 0
+  *
+  */
+
+ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return 0;
+}
