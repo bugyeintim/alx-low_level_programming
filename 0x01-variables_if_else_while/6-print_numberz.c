f45e7cb32f28f6ce301c039d500d6c5781f4bb89
Kofi Ntim <bugyeintim@outlook.com>
2023-10-07 05:35:11 -0700

    prints numbers 0 to 9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-
- /**
-  *
-  * main - prints digits from 0 - 9
-  *
-  * Return: Always 0 (Success)
-  *
-  */
+/**
+ * main - this prints numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
 		putchar('0' +  i);
 	}
