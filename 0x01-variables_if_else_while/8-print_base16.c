b0913df2275ca83e21fdf6e3ea07d5e02576d192
Kofi Ntim <bugyeintim@outlook.com>
2023-10-07 05:56:45 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase,
+ * and letters between a and f.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
