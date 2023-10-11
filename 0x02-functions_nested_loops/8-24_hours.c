b03c557fbab8fcd808a91e230348a096bb1177d2
Kofi Ntim <bugyeintim@outlook.com>
2023-10-11 15:20:59 -0700

    jack bauer code

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - entry point
+ *
+ * Description: function that prints every minute of
+ *  the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
+ *  Return: 0 if successfu
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a <= 2; a++)
+	{
+	for (b = 0; b <= 9; b++)
+	{
+	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+	{
+	for (c = 0; c <= 5; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+	_putchar(a + '0');
+	_putchar(b + '0');
+	_putchar(58);
+	_putchar(c + '0');
+	_putchar(d + '0');
+	_putchar('\n');
+	}
+	}
+	}
+	}
+	}
+}
