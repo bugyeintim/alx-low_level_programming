f33d925d630fe77986c2166dbd9369480bce7a8a
Kofi Ntim <bugyeintim@outlook.com>
2023-10-10 22:29:11 -0700

    checks if an alphabet is lowercase or not

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+
+/**
+ * _islower - function that checks for lowercase character
+ * @c: is the character to be checked
+ *
+ * Return:1 if char is lowercase otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c >= 'z')
+		return (1);
+	else
+		return (0);
+}
