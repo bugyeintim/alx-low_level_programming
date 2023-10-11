6c06eefa165a5bba19e6f60724212eaf09d1b7f3
Kofi Ntim <bugyeintim@outlook.com>
2023-10-11 15:28:35 -0700

    adds integers and gets results

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ *
+ *Description: function that adds two integers and
+ * returns the result.
+ *
+ * @n1: integer on
+ * @n2: integer two
+ *
+ * Return:sum.
+ *
+ */
+
+int add(int n1, int n2)
+{
+	int sum;
+
+	sum = n1 + n2;
+	return (sum);
+}
