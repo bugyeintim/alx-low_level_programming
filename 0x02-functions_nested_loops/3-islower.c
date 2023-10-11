fb4db02f114b5db05f064d44f6fda62707d4189f
Kofi Ntim <bugyeintim@outlook.com>
2023-10-11 07:41:29 -0700

    checks if it is upper or lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,11 +4,11 @@
  * _islower - function that checks for lowercase character
  * @c: is the character to be checked
  *
- * Return:1 if char is lowercase otherwise 0
+ * Return: 1 if char is uppercase and 0 if otherwise
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c >= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
