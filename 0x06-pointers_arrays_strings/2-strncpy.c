962c2e0366f69818f0b06b14232705e38a818e90
Kofi Ntim <bugyeintim@outlook.com>
2023-10-19 10:37:17 -0700

    task two refix

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,15 +10,18 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (i < n && src[i] != '\0')
 	{
-		while (i < n)
-		{
-			dest[i] = '\0';
-			i++;
-		}
+		dest[i] = src[i];
+		i++;
 	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
