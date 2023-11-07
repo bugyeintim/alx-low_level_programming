94958e781ba129f3aeaf45b4316e0053a0ef5ca6
Kofi Ntim <bugyeintim@outlook.com>
2023-11-07 12:28:41 -0800

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("File: %s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,3 @@
+# 0x0D. C - Preprocessor
+
+This is the documentation for the preprocessor project and the tasks.
