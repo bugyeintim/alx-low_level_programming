6bc15227ed1ba613c4c71020566173056af80168
Kofi Ntim <bugyeintim@outlook.com>
2023-11-08 14:08:56 -0800

    Task 3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,7 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+
 /**
  * struct op - Struct op
  *
@@ -9,3 +13,13 @@ typedef struct op
     char *op;
     int (*f)(int a, int b);
 } op_t;
+
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
