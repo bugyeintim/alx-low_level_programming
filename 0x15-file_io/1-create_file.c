b57a66d9656ce3306065cef4e4ccf27dee3b445e
Kofi Ntim <bugyeintim@outlook.com>
2023-11-28 11:29:28 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file with specific permissions and writes content
+ * @filename: name of the file to create
+ * @text_content: NULL-terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
