9203fdccb8fea8665dc1be21ca8029d46b65a36a
Kofi Ntim <bugyeintim@outlook.com>
2023-12-06 11:48:22 +0000

    modified task 0

---
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * _pow - Calculates (base ^ power)
+ * @base: Base of the exponent
+ * @power: Power of the exponent
+ *
+ * Return: Value of (base ^ power)
+ */
+unsigned long int _pow(unsigned int base, unsigned int power)
+{
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
+}
+
+/**
+ * print_binary - Prints a number in binary notation
+ * @n: Number to print in binary
+ *
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
+	{
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag == 1 || divisor == 1)
+		{
+			_putchar('0');
+		}
+		divisor >>= 1;
+	}
+}
