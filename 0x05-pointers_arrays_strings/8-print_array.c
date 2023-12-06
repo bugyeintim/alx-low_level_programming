9766d481849b2e1290b83d53963301c9f94a0fce
Kofi Ntim <bugyeintim@outlook.com>
2023-12-06 10:36:43 +0000

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elements of an array
+ * @a: Array name
+ * @n: Number of elements to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+
+	printf("\n");
+}
